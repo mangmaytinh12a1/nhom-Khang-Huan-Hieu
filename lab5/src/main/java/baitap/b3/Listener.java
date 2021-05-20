a30af2c8b376972c5343e104eb19a492213228a6
huykhang2811 <79626799+huykhang2811@users.noreply.github.com>
2021-05-20 15:33:47 +0700

    Add files via upload

---
+++ lab5/src/main/java/baitap/b3/Listener.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baitap.b3;
+
+/**
+ *
+ * @author nhom 3
+public interface Listener {
+    public void addNumber(int number);
+}
+
